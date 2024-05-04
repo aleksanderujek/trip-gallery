ee573e1825c9ae8a055bb045c0f88c3df9b8d9c4
aleksanderujek <aleksander.ujek@gmail.com>
2024-05-04 23:32:51 +0200

    improvement: added pending component to show spinner on loading page

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,24 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex, Spinner } from '@chakra-ui/react'
 import { theme } from './theme/theme'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({
+  routeTree,
+  defaultPendingComponent: () => (
+    <Flex justifyContent="center" alignItems="center" height="100vh">
+      <Spinner
+        thickness='4px'
+        speed='0.65s'
+        emptyColor='gray.200'
+        color='custom-navy'
+        size='xl'
+      />
+    </Flex>
+  )
+})
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
